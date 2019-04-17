cda2fdf1efe7be5a00fe07ec2fa26875de1f00af
Thakur Mahendar Singh <thakurmahendar.singh@accoliteindia.com>
2019-04-17 09:30:31 +0530

    Added burger and layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
+
+const Layout = (props) => (
+    <Aux>
+        <div>Toolbar, sidebar, drawer</div>
+        <main className={classes.content}>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default Layout;
