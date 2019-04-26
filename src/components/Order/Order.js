77fa7040186232e586f57f0da783696c47b8f5d9
Thakur Mahendar Singh <thakurmahendar.singh@accoliteindia.com>
2019-04-26 13:00:47 +0530

    fetch and display the orders in the orders page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import classes from './Order.css';
 
-const Order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients: Salad(1)</p>
-        <p>price: <strong>Rs: 20</strong></p>
-    </div>
-);
+const Order = (props) => { 
+    const ingredients = [];
+
+    //trandforming a object into array.
+    for(let ingName in props.ingredients){
+        ingredients.push({
+            name: ingName,
+            amount: props.ingredients[ingName]
+        })
+    }
+
+    const ingredientsOutput = ingredients.map(ing => {
+        console.log(ing.name)
+        return <span 
+                key={ing.name}
+                style={
+                    {
+                        textTransform: 'capitalize',
+                        display: 'inline-block',
+                        margin: '0 8px',
+                        border: '1px solid #ccc',
+                        padding: '5px'
+                    }
+                }>{ing.name} ({ing.amount})</span>
+    })
+    return(
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>price: <strong>Rs: {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    )
+};
 
 export default Order;
